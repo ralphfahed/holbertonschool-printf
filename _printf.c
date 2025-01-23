765e7898d61f543e5e012ae3a07411ff41c8ada9
ralph <ralphh.fahed@gmail.com>
2025-01-23 08:41:47 +0000

    task 0

--- _printf.c
+++ _printf.c
@@ -1,177 +1,86 @@
-#include "main.h"
-#include <unistd.h>
 #include <stdarg.h>
-#include <limits.h>  /* For INT_MIN */
-
-/* Function prototypes */
-int print_char(va_list args);
-int print_string(va_list args);
-int print_number(va_list args);
+#include <unistd.h>
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to print.
- *
- * Return: 1 on success, -1 on error.
+ * print_number - Prints an integer to stdout
+ * @n: The integer to print
+ * Return: The number of characters printed
  */
-int _putchar(char c)
+int print_number(int n)
 {
-return (write(1, &c, 1));
+    int count = 0;
+    char digit;
+
+    if (n < 0)
+    {
+        count += write(1, "-", 1);
+        n = -n;
+    }
+    if (n / 10)
+        count += print_number(n / 10);
+    digit = (n % 10) + '0';
+    count += write(1, &digit, 1);
+
+    return (count);
 }
 
 /**
- * print_char - Handles the %c format specifier.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
- */
-int print_char(va_list args)
-{
-char c = va_arg(args, int);
-return (_putchar(c));
-}
-
-/**
- * print_string - Handles the %s format specifier.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
- */
-int print_string(va_list args)
-{
-char *str = va_arg(args, char *);
-int count = 0;
-
-if (!str)
-str = "(null)";
-
-while (*str)
-{
-count += _putchar(*str);
-str++;
-}
-return (count);
-}
-
-/**
- * print_number - Handles the %d and %i format specifiers.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
- */
-int print_number(va_list args)
-{
-int n = va_arg(args, int);
-int count = 0;
-char buffer[50];
-int i = 0;
-
-if (n == 0)
-{
-count += _putchar('0');
-return (count);
-}
-
-if (n == INT_MIN)  /* Handle INT_MIN separately */
-{
-count += _putchar('-');
-count += _putchar('2');
-n = 147483648;  /* Adjust the number */
-}
-
-if (n < 0)
-{
-count += _putchar('-');
-n = -n;
-}
-
-while (n > 0)
-{
-buffer[i] = (n % 10) + '0';
-n /= 10;
-i++;
-}
-
-while (i > 0)
-{
-count += _putchar(buffer[--i]);
-}
-
-return (count);
-}
-
-/**
- * handle_specifier - Handles format specifiers.
- * @format: The format string.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
- */
-int handle_specifier(const char *format, va_list args)
-{
-int count = 0;
-
-if (*format == '%' && *(format + 1) == '\0')
-{
-return (count); /* No output for "%" alone */
-}
-
-switch (*format)
-{
-case 'c':
-count += print_char(args);
-break;
-case 's':
-count += print_string(args);
-break;
-case 'd':
-case 'i':
-count += print_number(args);
-break;
-case '%':
-count += _putchar('%');
-break;
-default:
-count += _putchar('%');
-count += _putchar(*format);
-break;
-}
-return (count);
-}
-
-/**
- * _printf - Produces output according to a format.
- * @format: The format string.
- *
- * Return: The number of characters printed.
+ * _printf - Produces output according to a format
+ * @format: The format string containing the characters and specifiers
+ * Return: The number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int i = 0, count = 0;
+    va_list args;
+    int count = 0, i;
 
-if (!format)
-return (-1);
+    if (!format)
+        return (-1);
 
-va_start(args, format);
+    va_start(args, format);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == '%')  /* Handle %% */
+            {
+                count += write(1, "%", 1);
+            }
+            else if (format[i] == 'c')  /* Handle %c */
+            {
+                char c = (char)va_arg(args, int);
+                count += write(1, &c, 1);
+            }
+            else if (format[i] == 's')  /* Handle %s */
+            {
+                char *str = va_arg(args, char *);
+                int len = 0;
 
-while (format[i])
-{
-if (format[i] == '%')
-{
-i++;
-if (format[i] == '\0')
-break;
-count += handle_specifier(&format[i], args);
-}
-else
-{
-count += _putchar(format[i]);
-}
-i++;
-}
+                if (!str)
+                    str = "(null)";
+                while (str[len] != '\0')
+                    len++;
+                count += write(1, str, len);
+            }
+            else if (format[i] == 'd' || format[i] == 'i')  /* Handle %d and %i */
+            {
+                int num = va_arg(args, int);
+                count += print_number(num);
+            }
+            else  /* Handle unknown specifiers */
+            {
+                count += write(1, "%", 1);
+                count += write(1, &format[i], 1);
+            }
+        }
+        else
+        {
+            count += write(1, &format[i], 1);
+        }
+    }
+    va_end(args);
 
-va_end(args);
-return (count);
+    return (count);
 }
 
